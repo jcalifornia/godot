6efd593b3bd2ab1df72dbb748f25d7377eb816cf
psuedo finnish <psuedofinnish@psuedos-MacBook-Pro.local>
2017-10-21 16:30:06 -0700

    added changes for dlib motion_sense to build on osx and I added pkgs install for osx:

--- modules/motion_sense/config.py
+++ modules/motion_sense/config.py
@@ -1,12 +1,15 @@
 # config.py
 
 def can_build(platform):
-    return True
+	return True
 
 #TODO: needs to be tested on windows
 def configure(env):
-    env.Append(LIBS=["opencv_core", "opencv_highgui"]) 
-    env.Append(CXXFLAGS=['-mavx'])
+	env.Append(LIBS=["opencv_core", "opencv_highgui"]) 
+	env.Append(CXXFLAGS=['-mavx'])
 
-    #env.Append(CCFLAGS=["-DLIB_NO_GUI_SUPPORT=1", "-DUSE_AVX_INSTRUCTIONS=1" ])
-    #env.ParseConfig('pkg-config opencv --cflags --libs')
+	#env.Append(CCFLAGS=["-DLIB_NO_GUI_SUPPORT=1", "-DUSE_AVX_INSTRUCTIONS=1" ])
+	#env.ParseConfig('pkg-config opencv --cflags --libs')
+	if env['platform'] == 'osx':
+		env.Append(CPPPATH=["/opt/X11/include/"])
+		env.Append(LIBS=["X11"]) 
