64796de7a551ec8e4983b990c87af069c9b59c60
hungrymonkey <psuedofinnish@psuedos-MBP.lan>
2017-10-24 11:50:21 -0700

    init commit talking tree, non works but compiles

---
+++ modules/talkingtree/register_types.cpp
@@ -0,0 +1,13 @@
+
+/* register_types.cpp */
+
+#include "register_types.h"
+#include "class_db.h"
+
+void register_talkingtree_types() {
+    
+}
+
+void unregister_talkingtree_types() {
+    //nothing to do here
+}
