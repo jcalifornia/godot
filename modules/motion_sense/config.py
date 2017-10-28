1f356be7e84f615c91fc61e49579e39ddc2028f1
hungrymonkey <boringmonkey@gmail.com>
2017-10-28 16:57:02 -0700

    added some code to allow it to build on linux, I did pkg-config on osx but not Linux. I just manually linked the libs

--- modules/motion_sense/config.py
+++ modules/motion_sense/config.py
@@ -5,7 +5,7 @@ def can_build(platform):
 
 #TODO: needs to be tested on windows
 def configure(env):
-	env.Append(LIBS=["opencv_core", "opencv_highgui"]) 
+	env.Append(LIBS=["opencv_video","opencv_imgproc","opencv_objdetect","opencv_core", "opencv_highgui"]) 
 	env.Append(CXXFLAGS=['-mavx'])
 
 	#env.Append(CCFLAGS=["-DLIB_NO_GUI_SUPPORT=1", "-DUSE_AVX_INSTRUCTIONS=1" ])
